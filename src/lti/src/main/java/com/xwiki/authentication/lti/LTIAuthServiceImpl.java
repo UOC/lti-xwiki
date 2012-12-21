f0afbe1a831f86e9857f26a231624c6173951bab
Jordi Duran Cals <jdurancal@CPU2716.(none)>
2012-12-21 16:35:50 +0100

    - versió 1.9.0: -- [SpeakApps] amb exportació PDF -- [LTI] amb creació de grups

--- src/lti/src/main/java/com/xwiki/authentication/lti/LTIAuthServiceImpl.java
+++ src/lti/src/main/java/com/xwiki/authentication/lti/LTIAuthServiceImpl.java
@@ -2,6 +2,7 @@ package com.xwiki.authentication.lti;
 
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.plugin.userdirectory.Group;
 import com.xpn.xwiki.user.impl.xwiki.XWikiAuthServiceImpl;
 import com.xpn.xwiki.web.XWikiServletRequest;
 import edu.uoc.lti.LTIEnvironment;
@@ -32,8 +33,11 @@ public class LTIAuthServiceImpl extends XWikiAuthServiceImpl {
                 if (usernameLTI.contains(":")) {
                     usernameLTI = usernameLTI.split(":")[1];
                 }
+                // get the group to assigna to the user.
+                String group = LTIEnvironment.getParameter("custom_groups");
+                System.out.println("GroupsLTI:"+group);
                 
-                return syncUser(usernameLTI, context);
+                return syncUser(usernameLTI, group, context);
             } else {
                 Exception lastException = LTIEnvironment.getLastException();
                 log.debug("Error LTI authentication "+(lastException!=null?lastException.getMessage():""));
@@ -55,7 +59,7 @@ public class LTIAuthServiceImpl extends XWikiAuthServiceImpl {
      * @param context
      * @throws com.xpn.xwiki.XWikiException
      */
-    protected Principal syncUser(String user, XWikiContext context) throws XWikiException {
+    protected Principal syncUser(String user, String groupName, XWikiContext context) throws XWikiException {
         String xwikiUser = super.findUser(user, context);
         if (xwikiUser == null) {
             log.debug("LTI Create user: User " + user + " does not exist");
@@ -63,8 +67,29 @@ public class LTIAuthServiceImpl extends XWikiAuthServiceImpl {
             context.getWiki().createEmptyUser(wikiname, "edit", context);
             log.debug("LTI Create user: User " + user + " has been created");
             xwikiUser = "XWiki."+user;
-            
             //TODO: passar-hi els paràmetres d'email, nom, etc..
+        }
+
+        // És un group però no és l'administrador
+        if (groupName != null && !groupName.equalsIgnoreCase("XWikiAdminGroup")) {
+            System.out.println("add member " + xwikiUser);
+            log.info("add member " + xwikiUser);
+            Group group = Group.getGroup("XWiki", groupName, context);
+            if (group != null && !group.isMember(xwikiUser, context)) {
+                System.out.println("exist group " + groupName);
+                log.info("exist group " + groupName);
+                if (group.addUser(user, context)) {
+                    System.out.println("is add");
+                    try {
+                        group.save(context);
+                    } catch (Exception e) {
+                        log.warn(e);
+                    }
+                }
+            } else {
+                System.out.println("no exist group " + groupName);
+                log.info("no exist group " + groupName);
+            }
         }
         
         return new SimplePrincipal(context.getDatabase() + ":" + xwikiUser);
